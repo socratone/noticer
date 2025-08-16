a20082b7eb3d9793557477704b88d56b46a96b49
Kiwon Kim <gim2origin@gmail.com>
2025-08-16 19:05:24 +0900

    feat: text field에 fullWidth prop 추가

--- src/components/atom/TextField/TextField.tsx
+++ src/components/atom/TextField/TextField.tsx
@@ -1,3 +1,4 @@
+import clsx from 'clsx';
 import styles from './TextField.module.css';
 import { forwardRef } from 'react';
 
@@ -6,14 +7,25 @@ interface TextFieldProps {
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
   name?: string;
+  fullWidth?: boolean;
+  className?: string;
 }
 
 const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
-  ({ placeholder, value, onChange, name, ...props }, ref) => {
+  (
+    { placeholder, value, onChange, name, fullWidth, className, ...props },
+    ref
+  ) => {
     return (
       <input
         ref={ref}
-        className={styles.input}
+        className={clsx(
+          styles.input,
+          {
+            [styles.fullWidth]: fullWidth,
+          },
+          className
+        )}
         type="text"
         placeholder={placeholder}
         value={value}
