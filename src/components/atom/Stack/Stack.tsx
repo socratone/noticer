52c906192eb196b6a045b066d5db22a1b9c710db
Kiwon Kim <gim2origin@gmail.com>
2025-08-16 18:12:09 +0900

    feat: alignItems과 justifyContent prop 추가

--- src/components/atom/Stack/Stack.tsx
+++ src/components/atom/Stack/Stack.tsx
@@ -4,11 +4,28 @@ interface StackProps {
   children: React.ReactNode;
   direction?: 'row' | 'column';
   gap?: number;
+  alignItems?: 'flex-start' | 'flex-end' | 'center' | 'baseline' | 'stretch';
+  justifyContent?:
+    | 'flex-start'
+    | 'flex-end'
+    | 'center'
+    | 'space-between'
+    | 'space-around'
+    | 'space-evenly';
 }
 
-const Stack = ({ children, direction = 'column', gap = 0 }: StackProps) => {
+const Stack = ({
+  children,
+  direction = 'column',
+  gap,
+  alignItems,
+  justifyContent,
+}: StackProps) => {
   return (
-    <div className={styles.stack} style={{ flexDirection: direction, gap }}>
+    <div
+      className={styles.stack}
+      style={{ flexDirection: direction, gap, alignItems, justifyContent }}
+    >
       {children}
     </div>
   );
