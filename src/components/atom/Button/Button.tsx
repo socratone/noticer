1d1622b7f90ff886a5e3b4518b2141ca04b8b0a8
Kiwon Kim <gim2origin@gmail.com>
2025-08-16 17:32:32 +0900

    feat: react-took-form 라이브러리와 호환되도록 업데이트

--- src/components/atom/Button/Button.tsx
+++ src/components/atom/Button/Button.tsx
@@ -3,11 +3,12 @@ import styles from './Button.module.css';
 interface ButtonProps {
   children: React.ReactNode;
   onClick?: React.MouseEventHandler<HTMLButtonElement>;
+  type?: 'button' | 'submit' | 'reset';
 }
 
-const Button = ({ children, onClick }: ButtonProps) => {
+const Button = ({ children, onClick, type = 'button' }: ButtonProps) => {
   return (
-    <button className={styles.button} onClick={onClick}>
+    <button className={styles.button} onClick={onClick} type={type}>
       {children}
     </button>
   );
--- src/components/atom/TextField/TextField.tsx
+++ src/components/atom/TextField/TextField.tsx
@@ -1,21 +1,28 @@
 import styles from './TextField.module.css';
+import { forwardRef } from 'react';
 
 interface TextFieldProps {
   placeholder?: string;
   value?: string;
   onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void;
+  name?: string;
 }
 
-const TextField = ({ placeholder, value, onChange }: TextFieldProps) => {
-  return (
-    <input
-      className={styles.input}
-      type="text"
-      placeholder={placeholder}
-      value={value}
-      onChange={onChange}
-    />
-  );
-};
+const TextField = forwardRef<HTMLInputElement, TextFieldProps>(
+  ({ placeholder, value, onChange, name, ...props }, ref) => {
+    return (
+      <input
+        ref={ref}
+        className={styles.input}
+        type="text"
+        placeholder={placeholder}
+        value={value}
+        onChange={onChange}
+        name={name}
+        {...props}
+      />
+    );
+  }
+);
 
 export default TextField;
